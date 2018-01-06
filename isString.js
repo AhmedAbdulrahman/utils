28f16c1c63674ab51c5ebeb841762cb106723d7c
Ahmed Abdulrahman <a.kasapbashi@live.com>
2018-01-06 18:24:47 +0100

    change made by request of the Manager

--- isString.js
+++ isString.js
@@ -1,6 +1,6 @@
 function isString(text) {
   return typeof text == 'string' || text insfanceof String;
 }
-function getUniques(arr) {
-  return Array.from(new Set(arr))
+function getUniques(arr2) {
+  return Array.from(new Set(arr2))
 }
